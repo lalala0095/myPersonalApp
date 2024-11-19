a6ed25b1ddce4b592f21d5386bf3ebcfd95f073d
Lemuel <torrefiellemuel@gmail.com>
2024-11-19 09:33:05 +0800

    Remove config.py

--- app/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-SECRET_KEY = os.getenv('SECRET_KEY', 'deb7b84457baecb6f3dffd832911412f8f448700') 
-MONGO_URI = os.getenv('MONGO_URI', "mongodb+srv://torrefiellemuel:wDp5H4BQSl8iAZ16@clusteradvpos.vixng.mongodb.net/")
